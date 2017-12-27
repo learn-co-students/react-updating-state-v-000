3de377c44848d4f121ffdfcc7d125992c8d94280
NickisKnowledge <lifechangingstuff@gmail.com>
2017-12-27 15:24:01 -0600

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -5,30 +5,19 @@ class ClickityClick extends React.Component {
   constructor() {
     super();
 
-    // define the initial state:
     this.state = {
       hasBeenClicked: false,
       currentTheme: 'blue',
     };
   }
 
-  // handleClick = () => {
-  //   //update our state here
-  //   this.setState({ hasBeenClicked: true });
-  //   // State changes, dnt happen asynchronously.
-  //   console.log(this.state.hasBeenClicked); // false after the states been updated
-  // };
-
-  // access our new state after it has been updated,
-  // we can optionally add a callback as a second argument
   handleClick = () => {
     this.setState({
       hasBeenClicked: true },
-
-      // second argument
-      () => console.log(this.state.hasBeenClicked) // prints true
+      () => console.log(this.state.hasBeenClicked)
     )
-  }
+  };
+
   render() {
     return (
       <div>
