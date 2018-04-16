df39eda6c53c0d63fa22dbc924a833e5c8b91826
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-04-16 23:54:37 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// ReactDOM.render(
+//   <div>
+//     Mount Components Here
+//   </div>,
+//   document.getElementById('root')
+// );
+
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+ 
+import ClickityClick from './components/ClickityClick';
+ 
 ReactDOM.render(
-  <div>
-    Mount Components Here
-  </div>,
+  <ClickityClick />,
   document.getElementById('root')
 );
