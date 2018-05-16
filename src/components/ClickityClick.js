da3ab28af2e15844d5ddb63c7e486f02d00b9d8f
ingridwong0715 <ingrid830715@gmail.com>
2018-05-16 14:10:30 -0400

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,32 @@
-// Code ClickityClick Component Here
+import React from 'react'
+
+class ClickityClick extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      hasBeenClicked: false
+    };
+  }
+  handleClick = () => {
+    this.setState({hasBeenClicked: true}); //WHY DOES IT HAVE TO BE ARROW FUNCTION?
+  } // the event handler is a CALLBACK 
+
+  //
+  // handleClick(){
+  //   this.setState({hasBeenClicked: true});
+  //   // this is undefined...?
+  // }
+
+  render(){
+    debugger;
+    return(
+      <div>
+        <p> I have {this.state.hasBeenClicked ? null : 'not'} been clicked! </p>
+
+        <button onClick={this.handleClick}>Click Me!</button>
+      </div>
+    );
+  }
+}
+
+export default ClickityClick;
