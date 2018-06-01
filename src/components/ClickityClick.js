bf24a65444b00e7687c200b3b6645e50e9c7aaaa
lawrend <douglas_lawrence@mac.com>
2018-05-31 23:30:35 -0700

    done

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,69 @@
-// Code ClickityClick Component Here
+import React from 'react';
+
+class ClickityClick extends React.Component {
+    constructor() {
+        super();
+
+        //Define the initial state:
+        this.state = {
+            hasBeenClicked: false,
+            currentTheme: 'blue',
+            addressInfo: {
+                street: null,
+                number: null,
+                city: null,
+                country: null
+              },
+        };
+    }
+
+    handleClick = () => {
+       this.setState({
+         hasBeenClicked: true
+       })
+     }
+
+    render () {
+        return (
+        <div>
+            <p>I have {this.state.hasBeenClicked ? null : 'not'} been clicked!</p>
+            <button onClick={this.handleClick}>Click me!</button>
+        </div>
+        )};
+}
+
+export default ClickityClick;
+
+//how to deep merge - two ways
+
+// use object assign
+this.setState({
+  addressInfo: Object.assign({}, this.state.addressInfo, {
+    city: 'New York City',
+  }),
+});
+
+// or recommended way
+this.setState({
+  addressInfo: {
+    ...this.state.addressInfo,
+    city: 'New York City',
+  },
+});
+
+//setting state is asynchronus so...
+handleClick = () => {
+  this.setState({
+    hasBeenClicked: true
+  })
+  console.log(this.state.hasBeenClicked); // prints false
+}
+
+//but this...
+handleClick = () => {
+  this.setState({
+    hasBeenClicked: true
+  }, () => console.log(this.state.hasBeenClicked)) // prints true
+}
+
+
