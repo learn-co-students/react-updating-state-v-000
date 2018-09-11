84d6fb870a5a3c56d3f6e2cfba21e6cc0e5cfd39
Sebastian <sebgertz@gmail.com>
2018-09-11 13:14:44 -0400

    finished readme

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import ClickityClick from './components/ClickityClick';
+
 ReactDOM.render(
-  <div>
-    Mount Components Here
-  </div>,
+  <ClickityClick />,
   document.getElementById('root')
 );
