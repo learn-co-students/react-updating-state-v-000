2865256c5e8d3945fffdfdf805d70ba27845b795
github username <github email address>
2020-07-16 21:38:06 -0700

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,39 @@
 // Code ClickityClick Component Here
+import React { Component } from 'react'
+
+class ClickityClick extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      hasBeenClicked: false
+    };
+  }
+
+    handleClick = () => {
+      this.setState({
+        hasBeenClicked: true
+      })
+    }
+
+
+    render() {
+      return (
+        <div>
+          <p> I have {this.state.hasBeenClicked ? null : 'not'}  </p>
+          <button onClick={this.handleClick}>Click me!</button>
+        </div>
+      );
+    }
+}
+
+export default ClicketyClick;
+
+
+
+//
+// notes-
+// -you define the initial state within the constructor, surprising
+// -syntax/code to set the initial state; i would have thought it was to use setstate. but
+// that's just for updating
+// -in the index file, the syntax for importing component since it's in a different folder
