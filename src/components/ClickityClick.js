abb4399507c360a0364f13f5edb71d91ff8cb20f
Eric Johnson <ericjohnson0216@icloud.com>
2021-01-08 19:05:43 -0500

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,14 @@
 // Code ClickityClick Component Here
+import React, { Component } from 'react';
+
+    class ClickityClick extends Component {
+
+        render() {
+            return(
+                <div></div>
+            )
+        }
+    
+    }
+
+export default ClickityClick;
